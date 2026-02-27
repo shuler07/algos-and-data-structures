053f61d9b3ef7f946432ce5bed83260e400c7257
shuler7 <vitalisobolevggg@gmail.com>
2026-02-27 18:42:05 +0300

    add lab1.h file for tree implementation

---
+++ lab1.h
@@ -0,0 +1,27 @@
+#ifndef lab1_h
+#define lab1_h
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+const int TREE_MAX_CHILDRENS = 5;
+
+typedef struct Node {
+    Node *parent;
+    Node *children[TREE_MAX_CHILDRENS];
+    int children_count;
+    int value;
+} Node;
+
+typedef struct {
+    Node *root;
+} Tree;
+
+void tree_init(Tree* tree, int value);
+bool tree_add(Tree *tree, int parent, int value);
+bool tree_remove(Tree *tree, int value);
+void tree_print(Tree *tree, int space_increment);
+int tree_peaks(Tree *tree);
+
+#endif
