85891a453df3aa4c062f962ebcd0d7f15fe19408
shuler07 <vitalisobolevggg@gmail.com>
2026-03-18 15:34:27 +0300

    add command to print expr tokens

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -4,12 +4,14 @@
 
 
 void show_system_message(bool is_expr_entered) {
-    printf("[ Expression not entered ]\n");
+    if (is_expr_entered) printf("[ Expression entered ]\n");
+    else printf("[ Expression not entered ]\n");
     printf("[ 1 ] Enter expression\n");
     printf("[ 2 ] Print expression\n");
     printf("[ 3 ] Print postfix expression\n");
-    printf("[ 4 ] Print expression as tree\n");
-    printf("[ 5 ] Remove unary minuses from multiplies\n");
+    printf("[ 4 ] Print postfix expression tokens\n");
+    printf("[ 5 ] Print expression as tree\n");
+    printf("[ 6 ] Remove unary minuses from multiplies\n");
     printf("[ 0 ] Exit program\n");
 };
 
@@ -19,43 +21,53 @@ int main() {
 
     char *expr = NULL;
     char *postfix_expr;
+    
+    List expr_tokens;
+    list_create(&expr_tokens);
+
     Tree expr_tree;
     tree_create(&expr_tree);
+    char garbage;
 
-    char choice;
     while (true) {
         show_system_message(is_expr_entered);
-        scanf("%c", &choice);
+        int choice;
+        scanf("%d%c", &choice, &garbage);
         switch (choice) {
-            case '0': return 0;
-            case '1':
+            case 0: return 0;
+            case 1:
                 printf(" => Type expression: ");
                 expr = get_expr_string(&expr_size);
                 if (expr[0] == '\0') printf("<< Empty expression! >>\n");
                 else {
                     is_expr_entered = true;
-                    postfix_expr = get_postfix_expr(expr, expr_size);
+                    postfix_expr = get_postfix_expr(expr, expr_size, &expr_tokens);
                     postfix_expr_to_tree(postfix_expr, &expr_tree);
                 };
                 break;
-            case '2':
+            case 2:
                 if (!is_expr_entered) printf("<< Enter expression first! >>\n");
                 else printf(" => Expression: %s\n", expr);
                 break;
-            case '3':
+            case 3:
                 if (!is_expr_entered) printf("<< Enter expression first! >>\n");
                 else printf(" => Postfix expression: %s\n", postfix_expr);
                 break;
-            case '4':
+            case 4:
+                if (!is_expr_entered) printf("<< Enter expression first! >>\n");
+                else list_print(&expr_tokens);
+                break;
+            case 5:
                 if (!is_expr_entered) printf("<< Enter expression first! >>\n");
                 else tree_print(expr_tree.root, 0);
                 break;
-            case '5':
+            case 6:
                 if (!is_expr_entered) printf("<< Enter expression first! >>\n");
                 else {
                     tree_remove_unary_minuses(&expr_tree, expr_tree.root);
                     expr = tree_to_expr(expr_tree.root, expr_size);
-                    postfix_expr = get_postfix_expr(expr, expr_size);
+                    list_clear(&expr_tokens);
+                    postfix_expr = get_postfix_expr(expr, expr_size, &expr_tokens);
                 };
                 break;
             default:
