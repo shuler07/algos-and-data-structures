5a128fc30a0c639c518782d5ae1b5e127f58c6bf
shuler07 <vitalisobolevggg@gmail.com>
2026-03-18 14:52:15 +0300

    add list realization

---
+++ lab3/list.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lab3.h"
+
+
+void list_create(List *lst) {
+    lst->first = NULL;
+    lst->last = NULL;
+};
+
+void list_push_begin(List *lst, char *token) {
+    ListNode *node = (ListNode*)malloc(sizeof(ListNode));
+    if (!node) {
+        printf("<< Memory allocation error! >>\n");
+        return;
+    };
+    node->token = token;
+    if (!lst->first) {
+        node->prev = NULL;
+        node->next = NULL;
+        lst->first = node;
+        lst->last = node;
+        return;
+    };
+    node->prev = NULL;
+    node->next = lst->first;
+    lst->first = node;
+};
+
+void list_push_end(List *lst, char *token) {
+    ListNode *node = (ListNode*)malloc(sizeof(ListNode));
+    if (!node) {
+        printf("<< Memory allocation error! >>\n");
+        return;
+    };
+    node->token = token;
+    if (!lst->last) {
+        node->prev = NULL;
+        node->next = NULL;
+        lst->first = node;
+        lst->last = node;
+        return;
+    };
+    node->prev = lst->last;
+    node->next = NULL;
+    lst->last = node;
+};
+
+char *list_pop_begin(List *lst) {
+    if (!lst->first) return NULL;
+    ListNode *first_node = lst->first;
+    char *value = first_node->token;
+    lst->first = first_node->next;
+    if (!lst->first) lst->last = NULL;
+    free(first_node);
+    return value;
+};
+
+char *list_pop_end(List *lst) {
+    if (!lst->last) return NULL;
+    ListNode *last_node = lst->last;
+    char *value = last_node->token;
+    lst->last = last_node->next;
+    if (!lst->last) lst->first = NULL;
+    free(last_node);
+    return value;
+};
