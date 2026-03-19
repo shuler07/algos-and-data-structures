8e7e755579dfbc3bc65cf6ad9d4428ea2169da8c
shuler07 <vitalisobolevggg@gmail.com>
2026-03-19 22:57:26 +0300

    update tests

--- lab3/test_lab3.c
+++ lab3/test_lab3.c
@@ -6,76 +6,104 @@
 
 void test_expr_1() {
     char expr[16] = "a*(-b)*3";
+    List tokens;
+    list_create(&tokens);
+    printf("\nTesting 1 expression - %s\n", expr);
 
-    char *postfix_expr = get_postfix_expr(expr, 16);
-    assert(strcmp(postfix_expr, "ab~*3*") == 0);
+    char *postfix_expr = get_postfix_expr(expr, 16, &tokens);
+    char answer[7] = "ab~*3*";
+    assert(strcmp(postfix_expr, answer) == 0);
+    assert(strcmp(tokens.first->token, "a") == 0);
+    assert(strcmp(tokens.first->next->token, "b") == 0);
+    printf("[ + ] Postfix expression, tokens\n");
 
     Tree tree_expr;
     tree_create(&tree_expr);
-    postfix_expr_to_tree(postfix_expr, &tree_expr);
-    assert(tree_expr.root->val == '*');
-    assert(tree_expr.root->right->val == '3');
-    assert(tree_expr.root->left->val == '*');
-    assert(tree_expr.root->left->right->val == '~');
-    assert(tree_expr.root->left->right->right->val == 'b');
-    assert(tree_expr.root->left->left->val == 'a');
+    postfix_expr_to_tree(&tokens, &tree_expr);
+    assert(strcmp(tree_expr.root->val, "*") == 0);
+    assert(strcmp(tree_expr.root->right->val, "3") == 0);
+    assert(strcmp(tree_expr.root->left->val, "*") == 0);
+    assert(strcmp(tree_expr.root->left->right->val, "~") == 0);
+    assert(strcmp(tree_expr.root->left->right->right->val, "b") == 0);
+    assert(strcmp(tree_expr.root->left->left->val, "a") == 0);
+    printf("[ + ] Tree expression\n");
 
-    tree_remove_unary_minuses(&tree_expr, tree_expr.root);
-    assert(tree_expr.root->val == '~');
+    tree_remove_unary_minuses(tree_expr.root);
+    assert(strcmp(tree_expr.root->val, "~") == 0);
+    printf("[ + ] Tree procedure\n");
 
     char *final_expr = tree_to_expr(tree_expr.root, 16);
     assert(strcmp(final_expr, "-(a*b*3)") == 0);
+    printf("[ + ] Expression from tree\n");
 
     printf("Tests passed for expression 1\n");
 };
  
 void test_expr_2() {
-    char expr[16] = "-a*(-b)*5*d";
+    char expr[16] = "(-a*b)+5";
+    List tokens;
+    list_create(&tokens);
+    printf("\nTesting 2 expression - %s\n", expr);
 
-    char *postfix_expr = get_postfix_expr(expr, 16);
-    assert(strcmp(postfix_expr, "a~b~*5*d*") == 0);
+    char *postfix_expr = get_postfix_expr(expr, 16, &tokens);
+    char answer[7] = "a~b*5+";
+    assert(strcmp(postfix_expr, answer) == 0);
+    assert(strcmp(tokens.first->token, "a") == 0);
+    assert(strcmp(tokens.first->next->token, "~") == 0);
+    printf("[ + ] Postfix expression, tokens\n");
 
     Tree tree_expr;
     tree_create(&tree_expr);
-    postfix_expr_to_tree(postfix_expr, &tree_expr);
-    assert(tree_expr.root->val == '*');
-    assert(tree_expr.root->right->val == 'd');
-    assert(tree_expr.root->left->val == '*');
-    assert(tree_expr.root->left->right->val == '5');
-    assert(tree_expr.root->left->left->val == '*');
-    assert(tree_expr.root->left->left->right->val == '~');
-    assert(tree_expr.root->left->left->right->right->val == 'b');
-    assert(tree_expr.root->left->left->left->val == '~');
-    assert(tree_expr.root->left->left->left->right->val == 'a');
+    postfix_expr_to_tree(&tokens, &tree_expr);
+    assert(strcmp(tree_expr.root->val, "+") == 0);
+    assert(strcmp(tree_expr.root->right->val, "5") == 0);
+    assert(strcmp(tree_expr.root->left->val, "*") == 0);
+    assert(strcmp(tree_expr.root->left->right->val, "b") == 0);
+    assert(strcmp(tree_expr.root->left->left->val, "~") == 0);
+    assert(strcmp(tree_expr.root->left->left->right->val, "a") == 0);
+    printf("[ + ] Tree expression\n");
 
-    tree_remove_unary_minuses(&tree_expr, tree_expr.root);
-    assert(tree_expr.root->val != '~');
+    tree_remove_unary_minuses(tree_expr.root);
+    assert(strcmp(tree_expr.root->val, "~") != 0);
+    assert(strcmp(tree_expr.root->left->val, "~") == 0);
+    printf("[ + ] Tree procedure\n");
 
     char *final_expr = tree_to_expr(tree_expr.root, 16);
-    assert(strcmp(final_expr, "a*b*5*d") == 0);
+    assert(strcmp(final_expr, "-(a*b)+5") == 0);
+    printf("[ + ] Expression from tree\n");
 
     printf("Tests passed for expression 2\n");
 };
 
 void test_expr_3() {
     char expr[16] = "-a*3";
+    List tokens;
+    list_create(&tokens);
+    printf("\nTesting 3 expression - %s\n", expr);
 
-    char *postfix_expr = get_postfix_expr(expr, 16);
-    assert(strcmp(postfix_expr, "a~3*") == 0);
+    char *postfix_expr = get_postfix_expr(expr, 16, &tokens);
+    char answer[5] = "a~3*";
+    assert(strcmp(postfix_expr, answer) == 0);
+    assert(strcmp(tokens.first->token, "a") == 0);
+    assert(strcmp(tokens.first->next->token, "~") == 0);
+    printf("[ + ] Postfix expression, tokens\n");
 
     Tree tree_expr;
     tree_create(&tree_expr);
-    postfix_expr_to_tree(postfix_expr, &tree_expr);
-    assert(tree_expr.root->val == '*');
-    assert(tree_expr.root->right->val == '3');
-    assert(tree_expr.root->left->val == '~');
-    assert(tree_expr.root->left->right->val == 'a');
+    postfix_expr_to_tree(&tokens, &tree_expr);
+    assert(strcmp(tree_expr.root->val, "*") == 0);
+    assert(strcmp(tree_expr.root->right->val, "3") == 0);
+    assert(strcmp(tree_expr.root->left->val, "~") == 0);
+    assert(strcmp(tree_expr.root->left->right->val, "a") == 0);
+    printf("[ + ] Tree expression\n");
 
-    tree_remove_unary_minuses(&tree_expr, tree_expr.root);
-    assert(tree_expr.root->val == '~');
+    tree_remove_unary_minuses(tree_expr.root);
+    assert(strcmp(tree_expr.root->val, "~") == 0);
+    printf("[ + ] Tree procedure\n");
 
     char *final_expr = tree_to_expr(tree_expr.root, 16);
     assert(strcmp(final_expr, "-(a*3)") == 0);
+    printf("[ + ] Expression from tree\n");
 
     printf("Tests passed for expression 3\n");
 };
